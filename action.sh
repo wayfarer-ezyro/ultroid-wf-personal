0f7a86f335d2dff69d5dde33f95139c6c7e2b1a1
TechiError <error@notavailable.live>
2021-12-28 21:37:56 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 git clone -b dev https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid && ls
-ls
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+pip3 install -U -r req*
+pip3 install -U -r res*/sta*/opt*
+python3 -m pyUltroid
